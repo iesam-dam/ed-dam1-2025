9d93c7a8a39f1b6e68c07b2d5c0c98730682302b
Pablo <pabloblaz06@gmail.com>
2025-11-30 13:03:38 +0100

    creacion de la clase cliente

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import features.Cliente;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Cliente cliente = new Cliente();
+
+        cliente.setNombre("");
+        cliente.setApellidos("");
+        cliente.setNif("");
+        cliente.setDireccion("");
+        cliente.setLocalidad("");
+        cliente.setProvincia("");
+        cliente.setTelefono("");
+        cliente.setEmail("");
     }
 }
