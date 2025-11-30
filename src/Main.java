cbeb0fb58148f5144c233a0333fe7a36059a4331
alvar <alvarodaca@outlook.es>
2025-11-30 18:16:44 +0100

    crear entidad cliente

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import dominio.Cliente;
+
 public class Main {
     public static void main(String[] args) {
 
+        Cliente cliente = new Cliente();
+
+        cliente.setNombre(" ");
+        cliente.setApellidos(" ");
+        cliente.setNif(" ");
+        cliente.setDireccion(" ");
+        cliente.setLocalidad(" ");
+        cliente.setProvincia(" ");
+        cliente.setTelefono(" ");
+        cliente.setEmail(" ");
+
+        //visualizar valores
+        System.out.println(cliente.getNombre());
+        System.out.println(cliente.getApellidos());
+        System.out.println(cliente.getNif());
+        System.out.println(cliente.getDireccion());
+        System.out.println(cliente.getLocalidad());
+        System.out.println(cliente.getProvincia());
+        System.out.println(cliente.getTelefono());
+        System.out.println(cliente.getEmail());
     }
 }
---
+++ src/dominio/Cliente.java
@@ -0,0 +1,76 @@
+package dominio;
+
+public class Cliente {
+    private String nombre;
+    private String apellidos;
+    private String nif;
+    private String direccion;
+    private String localidad;
+    private String provincia;
+    private String telefono;
+    private String email;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
