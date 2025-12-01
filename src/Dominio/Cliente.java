21dd3bedb88709c3af1b6800cdf5d3a192a525bf
MNA <angel.martinnavas2323@gmail.com>
2025-12-01 18:23:16 +0100

    He creado la rama cliente y la entidad Cliente

---
+++ src/Dominio/Cliente.java
@@ -0,0 +1,78 @@
+package Dominio;
+
+public class Cliente {
+    private String Nombre;
+    private String Apellidos;
+    private String NIF;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        Apellidos = apellidos;
+    }
+
+    public String getNIF() {
+        return NIF;
+    }
+
+    public void setNIF(String NIF) {
+        this.NIF = NIF;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
+
+    public String getLocalidad() {
+        return Localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        Localidad = localidad;
+    }
+
+    public String getProvincia() {
+        return Provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        Provincia = provincia;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        Telefono = telefono;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    private String Direccion;
+    private String Localidad;
+    private String Provincia;
+    private String Telefono;
+    private String Email;
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import Dominio.Cliente;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+
+        cliente.setNombre("miguel");
+        cliente.setApellidos("");
+        cliente.setNIF("");
+        cliente.setDireccion("");
+        cliente.setLocalidad("");
+        cliente.setProvincia("");
+        cliente.setTelefono("");
+        cliente.setEmail("");
+
+
+        System.out.println(cliente.getNombre());
+        System.out.println(cliente.getApellidos());
+        System.out.println(cliente.getNIF());
+        System.out.println(cliente.getDireccion());
+        System.out.println(cliente.getLocalidad());
+        System.out.println(cliente.getProvincia());
+        System.out.println(cliente.getTelefono());
+        System.out.println(cliente.getEmail());
+
 
     }
 }
